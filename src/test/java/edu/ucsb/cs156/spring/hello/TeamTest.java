cb82015d2181ffdabba9521d889b79e4fc213047
Oscar Luna <oscar_luna@uscb.edu>
2025-04-09 09:33:07 -0700

    hash and equals test cases updated

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -2,6 +2,9 @@ package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -22,5 +25,57 @@ public class TeamTest {
    
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+    @Test
+    public void equals_case1(){
+        assertTrue(team.equals(team), "the same object shoudl equal" );
+    }
+    @Test
+    public void equals_case2(){
+        String temp = "team";
+        assertFalse(team.equals(temp), "Objects of differnt type cannot be equal");
+    }
+    @Test
+    public void equals_case3_1(){
+        Team temp;
+	temp = new Team("test-team");
+	assertTrue(team.equals(temp), "same name and same members should be equal");
+    }
+
+    @Test
+    public void equals_case3_2(){
+        Team temp;
+	temp = new Team("test-team");
+	temp.addMember("spy");
+	assertFalse(team.equals(temp), "same name and different members should be false");
+    }
+
+    @Test
+    public void equals_case3_3(){
+        Team temp;
+	temp = new Team("wrong-team");
+	assertFalse(team.equals(temp), "Different name and same members should not be equal");
+    }
+
+    @Test
+    public void equals_case3_4(){
+        Team temp;
+	temp = new Team("wrong-team");
+	temp.addMember("spy");
+	assertFalse(team.equals(temp), "wrong name and wrong members should not be equal");
+    }
+
+    @Test
+    public void hashCode_check(){
+        int result = team.hashCode();
+        int expectedResult = -1226298695;
+        assertEquals(expectedResult, result);
+    }
+
+
 
 }
